2add4bab7fdea8d264107115d3da76785fd65381
Dhanabalan Saravanan <115478237+way2sdhana@users.noreply.github.com>
2023-01-12 22:55:19 +0530

    Add files via upload

---
+++ formValid/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FValidate from './FValidate';
+// in this project we are using React-Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <br/>
+      <FValidate />
+      <br />
+    </>
+  );
+}
+export default App;
